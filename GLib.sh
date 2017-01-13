b540b92d005446b5d832b0c7522a9b9931b7792f
zoe <z.zoelec2@gmail.com>
2017-01-13 12:32:36 -0800

    adding gentoo PA

--- GLib.sh
+++ GLib.sh
@@ -1,7 +1,14 @@
 while true; do
     read -p "Do you wish to install this program?" yn
     case $yn in
-        [Yy]* ) wget "http://anduin.linuxfromscratch.org/BLFS/blfs-bootscripts/blfs-bootscripts-20160902.tar.xz"; make install-random; break;;
+        [Yy]* ) wget "http://ftp.gnome.org/pub/gnome/sources/glib/2.48/glib-2.48.2.tar.xz" &&
+        tar xf glib-2.48.2.tar.xz &&
+        cd glib-2.48.2 &&
+        ./configure --prefix=/usr --with-pcre=system &&
+        make &&
+        make install &&
+        cd ..;
+        break;;
         [Nn]* ) exit;;
         * ) echo "Please answer yes or no.";;
     esac
